12a343f5c0d7b090cc10fe4c99d2a0e371cb81d6
Sakshi <SakshiBhanudas.Pawar@cognizant.com>
2026-03-11 10:14:38 +0530

    app config changes

--- FinalProject_FE/src/app/app.config.ts
+++ FinalProject_FE/src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { authInterceptor } from './auth.interceptor';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
